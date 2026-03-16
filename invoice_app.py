bcaabf9d5c942620bb9dc2e8f9273854548ffc51
MagdaGHub <101306217+MagdaGHub@users.noreply.github.com>
2026-03-16 14:27:09 -0400

    Update invoice_app.py

--- invoice_app.py
+++ invoice_app.py
@@ -152,7 +152,8 @@ if not st.session_state.authenticated:
     """, unsafe_allow_html=True)
 
 # ADMIN ACCESS AFTER LOGIN
-with st.expander("Admin Access"):
+#with st.expander("Admin Access"):
+    st.markdown("#### Admin Access")
     top1, top2 = st.columns([3, 1])
         
     with top1:
