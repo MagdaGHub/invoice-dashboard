cb5700d778ed9fc9611410a6259b76415e8d092e
MagdaGHub <101306217+MagdaGHub@users.noreply.github.com>
2026-03-23 15:02:15 -0400

    Update invoice_app.py

--- invoice_app.py
+++ invoice_app.py
@@ -2306,7 +2306,7 @@ def render_reports_tab() -> None:
                 comparison_labels = (
                     alt.Chart(all_phase_actuals)
                     .transform_filter(alt.datum.actual_amount > 20000)
-                    .mark_text(angle=270, dy=0, dx=1)
+                    .mark_text(angle=270, dy=0, dx=5)
                     .encode(
                         x=alt.X("phase_label:N", sort=phase_order),
                         y=alt.Y("actual_amount:Q"),
